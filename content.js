09d18b5aae7782b7becbf5f18ae40b7ab977d24f
coresyntax0330 <coresyntax0330@gmail.conm>
2026-05-04 09:22:55 +0100

    -Update

--- content.js
+++ content.js
@@ -7,8 +7,8 @@
 
   const TARGET_CONTAINERS = [".job-ad-display-ofzx2"];
 
-  const API_URL = "http://45.15.160.247:5000/api/bids/get-draft";
-  const BID_PAGE_URL = "http://45.15.160.247:3000/bid";
+  const API_URL = "http://37.221.127.4:5000/api/bids/get-draft";
+  const BID_PAGE_URL = "http://37.221.127.4:3000/bid";
 
   // ---------- UI ----------
   const uiWrapper = document.createElement("div");
